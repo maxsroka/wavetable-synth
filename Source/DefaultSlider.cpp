a83a07b5dc49e0da550cbca9ed616adb16670ca6
maxsroka <36799862+maxsroka@users.noreply.github.com>
2026-04-18 13:44:55 +0200

    formatted components

--- Source/DefaultSlider.cpp
+++ Source/DefaultSlider.cpp
@@ -3,13 +3,13 @@
 
 DefaultSlider::DefaultSlider(DefaultStyle& style, double rangeMin, double rangeMax, double interval)
 {
-    this->setRange(rangeMin, rangeMax, interval);
-    this->setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
-    this->setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 16);
-    this->setLookAndFeel(&style);
+	this->setRange(rangeMin, rangeMax, interval);
+	this->setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+	this->setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 16);
+	this->setLookAndFeel(&style);
 }
 
 bool DefaultSlider::operator==(juce::Slider* slider)
 {
-    return slider->getComponentID() == this->getComponentID();
+	return slider->getComponentID() == this->getComponentID();
 }
