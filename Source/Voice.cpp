f6bdb8943d852ac212ef39cd08a1bbeb5fe16e32
maxsroka <36799862+maxsroka@users.noreply.github.com>
2026-04-18 13:39:33 +0200

    formatted voice

--- Source/Voice.cpp
+++ Source/Voice.cpp
@@ -1,34 +1,34 @@
 #include "Voice.h"
 
-Voice::Voice(Oscillator* oscillator, int noteNumber) : oscillator(oscillator), noteNumber(noteNumber) { }
+Voice::Voice(Oscillator* oscillator, int noteNumber) : oscillator(oscillator), noteNumber(noteNumber) {}
 
 void Voice::start()
 {
-    float noteFreq = juce::MidiMessage::getMidiNoteInHertz(noteNumber);
-    oscillator->setFrequency(noteFreq);
+	float noteFreq = juce::MidiMessage::getMidiNoteInHertz(noteNumber);
+	oscillator->setFrequency(noteFreq);
 }
 
 void Voice::stop()
 {
-    oscillator->setFrequency(0.0);
+	oscillator->setFrequency(0.0);
 }
 
 float Voice::getNextSample(float shape)
 {
-    return oscillator->getNextSample(shape) * VOICE_LEVEL;
+	return oscillator->getNextSample(shape) * VOICE_LEVEL;
 }
 
 int Voice::findIndexByNote(std::vector<Voice> voices, int noteNumber)
 {
-    for (int i = 0; i < voices.size(); ++i)
-    {
-        Voice& voice = voices[i];
+	for (int i = 0; i < voices.size(); ++i)
+	{
+		Voice& voice = voices[i];
 
-        if (voice.noteNumber == noteNumber)
-        {
-            return i;
-        }
-    }
+		if (voice.noteNumber == noteNumber)
+		{
+			return i;
+		}
+	}
 
-    return -1;
+	return -1;
 }
