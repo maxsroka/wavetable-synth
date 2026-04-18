4bc638c01be2859e5e348e9ab06c46419ac1f3cf
maxsroka <36799862+maxsroka@users.noreply.github.com>
2026-04-18 13:38:28 +0200

    formatted pluginprocessor

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin processor.
+	This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -12,218 +12,211 @@
 //==============================================================================
 WavetableSynthAudioProcessor::WavetableSynthAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       ),
-    parameters(*this, nullptr, "Parameters", createParameterLayout())
+	: AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+		.withInput("Input", juce::AudioChannelSet::stereo(), true)
 #endif
-{
+		.withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+	),
+	parameters(*this, nullptr, "Parameters", createParameterLayout())
+#endif
+{}
 
-}
-
-WavetableSynthAudioProcessor::~WavetableSynthAudioProcessor()
-{
-}
+WavetableSynthAudioProcessor::~WavetableSynthAudioProcessor() {}
 
 //==============================================================================
 const juce::String WavetableSynthAudioProcessor::getName() const
 {
-    return JucePlugin_Name;
+	return JucePlugin_Name;
 }
 
 bool WavetableSynthAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_WantsMidiInput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool WavetableSynthAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_ProducesMidiOutput
+	return true;
+#else
+	return false;
+#endif
 }
 
 bool WavetableSynthAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
+#if JucePlugin_IsMidiEffect
+	return true;
+#else
+	return false;
+#endif
 }
 
 double WavetableSynthAudioProcessor::getTailLengthSeconds() const
 {
-    return 0.0;
+	return 0.0;
 }
 
 int WavetableSynthAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+	return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+	// so this should be at least 1, even if you're not really implementing programs.
 }
 
 int WavetableSynthAudioProcessor::getCurrentProgram()
 {
-    return 0;
+	return 0;
 }
 
-void WavetableSynthAudioProcessor::setCurrentProgram (int index)
+void WavetableSynthAudioProcessor::setCurrentProgram(int index) {}
+
+const juce::String WavetableSynthAudioProcessor::getProgramName(int index)
 {
+	return {};
 }
 
-const juce::String WavetableSynthAudioProcessor::getProgramName (int index)
-{
-    return {};
-}
-
-void WavetableSynthAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+void WavetableSynthAudioProcessor::changeProgramName(int index, const juce::String& newName)
+{}
 
 //==============================================================================
-void WavetableSynthAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void WavetableSynthAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    wavetable.generate();
-    
+	wavetable.generate();
+
 	for (Oscillator& oscillator : oscillators)
-    {
-        oscillator.setup(&wavetable, sampleRate);
-    }
+	{
+		oscillator.setup(&wavetable, sampleRate);
+	}
 }
 
 void WavetableSynthAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+	// When playback stops, you can use this as an opportunity to free up any
+	// spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool WavetableSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool WavetableSynthAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+#if JucePlugin_IsMidiEffect
+	juce::ignoreUnused(layouts);
+	return true;
+#else
+	// This is the place where you check if the layout is supported.
+	// In this template code we only support mono or stereo.
+	// Some plugin hosts, such as certain GarageBand versions, will only
+	// load plugins that support stereo bus layouts.
+	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+		&& layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+		return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+	// This checks if the input layout matches the output layout
+#if ! JucePlugin_IsSynth
+	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+		return false;
+#endif
 
-    return true;
-  #endif
+	return true;
+#endif
 }
 #endif
 
-void WavetableSynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void WavetableSynthAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
-    for (auto element : midiMessages)
-    {
-        juce::MidiMessage msg = element.getMessage();
-        int noteNumber = msg.getNoteNumber();
+	for (auto element : midiMessages)
+	{
+		juce::MidiMessage msg = element.getMessage();
+		int noteNumber = msg.getNoteNumber();
 
-        if (msg.isNoteOn())
-        {
-            Oscillator* oscillator = Oscillator::findInactive(oscillators);
+		if (msg.isNoteOn())
+		{
+			Oscillator* oscillator = Oscillator::findInactive(oscillators);
 			if (oscillator == nullptr) continue;
-            
-            Voice voice = Voice(oscillator, noteNumber);
-            voice.start();
 
-            voices.push_back(voice);
-        }
-        else if (msg.isNoteOff())
-        {
-            int index = Voice::findIndexByNote(voices, noteNumber);
-            if (index == -1) continue;
+			Voice voice = Voice(oscillator, noteNumber);
+			voice.start();
 
-            voices[index].stop();
+			voices.push_back(voice);
+		}
+		else if (msg.isNoteOff())
+		{
+			int index = Voice::findIndexByNote(voices, noteNumber);
+			if (index == -1) continue;
 
-            voices.erase(voices.begin() + index);
-        }
-    }
+			voices[index].stop();
 
-    int numOutputChannels = getTotalNumOutputChannels();
-    int numSamples = buffer.getNumSamples();
-    float shape = parameters.getParameter("shape")->getValue();
+			voices.erase(voices.begin() + index);
+		}
+	}
 
-    for (int i = 0; i < numSamples; ++i)
-    {
-        float sample = 0.0f;
-        for (Voice& voice : voices)
-        {
-            sample += voice.getNextSample(shape);
-        }
+	int numOutputChannels = getTotalNumOutputChannels();
+	int numSamples = buffer.getNumSamples();
+	float shape = parameters.getParameter("shape")->getValue();
 
-        for (int channel = 0; channel < numOutputChannels; ++channel)
-        {
-            float* ptr = buffer.getWritePointer(channel);
-            ptr[i] = sample * 0.2f;
-        }
-    }
-    
-    midiMessages.clear();
-    buffer.applyGain(parameters.getParameter("volume")->getValue());
+	for (int i = 0; i < numSamples; ++i)
+	{
+		float sample = 0.0f;
+		for (Voice& voice : voices)
+		{
+			sample += voice.getNextSample(shape);
+		}
+
+		for (int channel = 0; channel < numOutputChannels; ++channel)
+		{
+			float* ptr = buffer.getWritePointer(channel);
+			ptr[i] = sample * 0.2f;
+		}
+	}
+
+	midiMessages.clear();
+	buffer.applyGain(parameters.getParameter("volume")->getValue());
 }
 
 //==============================================================================
 bool WavetableSynthAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+	return true; // (change this to false if you choose to not supply an editor)
 }
 
 juce::AudioProcessorEditor* WavetableSynthAudioProcessor::createEditor()
 {
-    return new WavetableSynthAudioProcessorEditor (*this, this->parameters);
+	return new WavetableSynthAudioProcessorEditor(*this, this->parameters);
 }
 
 //==============================================================================
-void WavetableSynthAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void WavetableSynthAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+	// You should use this method to store your parameters in the memory block.
+	// You could do that either as raw data, or use the XML or ValueTree classes
+	// as intermediaries to make it easy to save and load complex data.
 }
 
-void WavetableSynthAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void WavetableSynthAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+	// You should use this method to restore your parameters from this memory block,
+	// whose contents will have been created by the getStateInformation() call.
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return new WavetableSynthAudioProcessor();
+	return new WavetableSynthAudioProcessor();
 }
 
 juce::AudioProcessorValueTreeState::ParameterLayout WavetableSynthAudioProcessor::createParameterLayout()
 {
-    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+	juce::AudioProcessorValueTreeState::ParameterLayout layout;
 
-    layout.add(std::make_unique<juce::AudioParameterFloat>("volume", "Volume", juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 1.0f));
-    layout.add(std::make_unique<juce::AudioParameterFloat>("shape", "Shape", juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>("volume", "Volume", juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 1.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>("shape", "Shape", juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.0f));
 
-    return layout;
+	return layout;
 }
